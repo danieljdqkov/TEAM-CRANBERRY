61e86e5cf47b2163a1bdb8a466a1109dd769b81e
tsvetanov <tsvetanov.george@gmail.com>
2014-09-15 16:03:08 +0300

    button

---
+++ src/button.java
@@ -0,0 +1,16 @@
+package button;
+
+import java.awt.*;
+import java.applet.*;
+
+public class button extends Applet{
+	public void init(){
+		BorderLayout setLayout = (new BorderLayout());
+			Button Easy = new Button("Easy");
+			Button Normal = new Button("Normal");
+			Button Hard = new Button("Hard");
+				add(Easy,"North" );
+				add(Normal, "East");
+				add(Hard, "South");
+	}
+}
